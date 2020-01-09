7046fb29df00806771cfa165afa73b055e993c5a
Brandon Ring <W0432111@campus.nscc.ca>
2020-01-09 10:16:33 -0400

    Commit

---
+++ Introduction/cmake-build-debug/CMakeFiles/Introduction.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Introduction.dir/main.c.obj"
+  "Introduction.pdb"
+  "Introduction.exe"
+  "Introduction.exe.manifest"
+  "libIntroduction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Introduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
